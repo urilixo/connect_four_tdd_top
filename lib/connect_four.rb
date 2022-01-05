877251598777c8f637d72257c80196252acc533b
urilixo <falecomelvio@hotmail.com>
2022-01-05 20:31:58 -0300

    Add methods ,  and  to ConnectFour

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,48 @@
 class ConnectFour
+  attr_reader :player1, :player2, :board
+
+  def initialize(player1 = nil, player2 = nil)
+    @player1 = player_name(player1)
+    @player2 = player_name(player2)
+    @player_turn = @player1
+    @board = create_board
+  end
+
+  def drop_piece(player, column)
+    column -= 1
+    piece = player == @player1 ? '⚫' : '⚪'
+    (0..5).each do |i|
+      next if @board[i + 1] == '_'
+
+      if (@board[i + 1].nil? || @board[i + 1][column] != '_') && @board[i][column] == '_'
+        @board[i][column] = piece and return [i, column]
+      end
+    end
+    'Column is full. Try again!'
+  end
+
+  def end_turn(player)
+    player == @player1 ? @player2 : @player1
+  end
+
+  def check_victory
+  end
+
+  def player_name(name = nil)
+    name = gets.chomp if name.nil?
+    name
+  end
+
+  def create_board
+    [
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _]
+    ]
+  end
 end
+
+
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,22 +1,39 @@
+require_relative '../lib/connect_four'
+
 describe ConnectFour do
   describe '#drop_piece' do
-    subject(:game_drop) { described_class.new()}
+    subject(:game_drop) { described_class.new('p1', 'p2')}
     context 'when a player chooses an empty column' do
       it 'drops a piece down the choosen column' do
-        expect(game_drop.drop_piece(player1, 3)).to eq([5, 2])
+        expect(game_drop.drop_piece(game_drop.player1, 3)).to eq([5, 2])
       end
     end
 
     context 'when a player picks a half full column' do
+      before do
+        game_drop.drop_piece(game_drop.player1, 3)
+        game_drop.drop_piece(game_drop.player2, 3)
+        game_drop.drop_piece(game_drop.player1, 3)
+      end
+
       it 'drops a piece down a half filled column' do
-        expect(game_drop.drop_piece(player2, 3)).to eq([2, 2])
+        expect(game_drop.drop_piece(game_drop.player2, 3)).to eq([2, 2])
       end
     end
 
     context 'when a player tries to drop in a full column once and lets player try again' do
+      before do
+        game_drop.drop_piece(game_drop.player1, 3)
+        game_drop.drop_piece(game_drop.player2, 3)
+        game_drop.drop_piece(game_drop.player1, 3)
+        game_drop.drop_piece(game_drop.player2, 3)
+        game_drop.drop_piece(game_drop.player1, 3)
+        game_drop.drop_piece(game_drop.player2, 3)
+        game_drop.drop_piece(game_drop.player1, 3)
+      end
       it 'returns an error message and put piece in another chosen column' do
-        expect(game_drop.drop_piece(player1, 3)).to eq('Column is full. Try again!')
-        expect(game_drop.drop_piece(player1, 1)).to eq([5, 0])
+        expect(game_drop.drop_piece(game_drop.player1, 3)).to eq('Column is full. Try again!')
+        expect(game_drop.drop_piece(game_drop.player1, 1)).to eq([5, 0])
       end
     end
   end
@@ -25,7 +42,7 @@ describe ConnectFour do
     subject(:game_turn) { described_class.new('player A', 'player B')}
     context 'when turn ends' do
       it 'changes current_player to the other player' do
-        expect(end_turn(player1)).to eq(player2)
+        expect(game_turn.end_turn(game_turn.player1)).to eq(game_turn.player2)
       end
     end
   end
@@ -34,16 +51,16 @@ describe ConnectFour do
     subject(:game_victory) {described_class.new()}
     context 'when 4 tiles are connected horizontally' do
       before do
-        let(:h_board) { 
-          [['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '⚫', '⚫', '⚫', '⚫', '_']]
-        }
+        let(:h_board) {[
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ ⚫ ⚫ ⚫ ⚫]
+        ]}
       end
-      it 'returns victory for player 1' do
+      xit 'returns victory for player 1' do
         expect(game_victory).to receive(:victory)
         game_victory.check_victory(h_board)
       end
@@ -51,16 +68,16 @@ describe ConnectFour do
 
     context 'when 4 tiles are connected vertically' do
       before do
-        let(:v_board) { 
-          [['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '⚪', '_', '_', '_', '_', '_'],
-           ['_', '⚪', '_', '_', '_', '_', '_'],
-           ['_', '⚪', '_', '_', '_', '_', '_'],
-           ['_', '⚪', '_', '⚫', '⚫', '⚫', '_']]
-        }
+        let(:v_board) {[
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ ⚪ _ _ _],
+          %w[_ _ _ ⚪ _ _ _],
+          %w[_ _ _ ⚪ _ _ _],
+          %w[_ _ _ ⚪ ⚫ ⚫ ⚫]
+        ]}
       end
-      it 'returns victory for player 2' do
+      xit 'returns victory for player 2' do
         expect(game_victory).to receive(:victory)
         game_victory.check_victory(v_board)
       end
@@ -68,16 +85,16 @@ describe ConnectFour do
 
     context 'when 4 tiles are connected diagonally' do
       before do
-        let(:d_board) { 
-          [['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['⚫', '⚪', '_', '_', '_', '_', '_'],
-           ['⚪', '⚫', '_', '_', '_', '_', '_'],
-           ['⚪', '⚪', '⚫', '_', '_', '_', '_'],
-           ['⚪', '⚪', '⚪', '⚫', '⚫', '⚫', '_']]
-        }
+        let(:d_board) {[
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[_ ⚫ ⚪ ⚪ _ _ _],
+          %w[_ ⚪ ⚫ ⚪ _ _ _],
+          %w[_ ⚪ ⚪ ⚫ _ _ _],
+          %w[_ ⚪ ⚪ ⚪ ⚫ ⚫ ⚫]
+        ]}
       end
-      it 'returns victory for player 1' do
+      xit 'returns victory for player 1' do
         expect(game_victory).to receive(:victory)
         game_victory.check_victory(d_board)
       end
@@ -85,16 +102,16 @@ describe ConnectFour do
 
     context 'when there is no winning condition' do
       before do
-        let(:incomplete_board) { 
-          [['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '_', '_', '_', '_', '_', '_'],
-           ['_', '⚪', '_', '_', '_', '_', '_'],
-           ['⚪', '⚫', '_', '_', '_', '_', '_'],
-           ['⚪', '⚪', '⚫', '_', '_', '_', '_'],
-           ['⚫', '⚪', '⚪', '⚫', '⚫', '⚫', '_']]
-        }
+        let(:incomplete_board) {[
+          %w[_ _ _ _ _ _ _],
+          %w[_ _ _ _ _ _ _],
+          %w[⚫ ⚫ ⚪ ⚪ _ _ _],
+          %w[⚫ ⚪ ⚪ ⚪ _ _ _],
+          %w[⚪ ⚫ ⚫ ⚫ _ _ _],
+          %w[⚫ ⚪ ⚪ ⚪ ⚫ ⚫ ⚫]
+        ]}
       end
-      it 'returns nil' do
+      xit 'returns nil' do
         expect(game_victory.check_victory(incomplete_board)).to be_nil
       end
     end
